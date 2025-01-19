d77bda9674e00c61a800376bf6cb6dd1776eeb17
caruazu <gustavocaruazu@gmail.com>
2025-01-18 22:56:20 -0300

    página de cadastro

---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { LayoutComponent } from '../../components/layout/layout.component';
+import {
+  Form,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputPrimaryComponent } from '../../components/input-primary/input-primary.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+
+interface CadastroForm {
+  username: FormControl;
+  email: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
+}
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [LayoutComponent, ReactiveFormsModule, InputPrimaryComponent],
+  providers: [LoginService],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss',
+})
+
+export class CadastroComponent {
+  cadastroForm!: FormGroup<CadastroForm>;
+
+  constructor(private router: Router, private loginService: LoginService) {
+    this.cadastroForm = new FormGroup({
+      username: new FormControl('',[Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(12)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(12)]),
+    });
+  }
+
+  navigate() {
+    this.router.navigate([""])
+  }
+
+  submit() {
+    throw new Error('Method not implemented.');
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,6 +5,11 @@ import { InputPrimaryComponent } from '../../components/input-primary/input-prim
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl,
+}
+
 @Component({
   selector: 'app-login',
   imports: [LayoutComponent, ReactiveFormsModule, InputPrimaryComponent],
@@ -12,8 +17,9 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
+
 export class LoginComponent {
-  loginForm!: FormGroup;
+  loginForm!: FormGroup<LoginForm>;
 
   constructor(private router: Router, private loginService: LoginService) {
     this.loginForm = new FormGroup({
@@ -26,16 +32,15 @@ export class LoginComponent {
   }
 
   submit() {
-    this.loginService.login(
-      this.loginForm.value.email,
-      this.loginForm.value.password
-    ).subscribe({
-      next: () => console.log("sucesso"),
-      error: () => console.log("error")
-    })
+    this.loginService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe({
+        next: () => console.log('sucesso'),
+        error: () => console.log('error'),
+      });
   }
 
   navigate() {
-    this.router.navigate(["registrar"])
+    this.router.navigate(['cadastro']);
   }
 }
